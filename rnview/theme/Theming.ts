9e9c9197c44a11617045295ac027fac185b0e839
gabrielpovoa <povoajoao6@gmail.com>
2024-01-27 16:54:53 -0300

    creating theme

---
+++ rnview/theme/Theming.ts
@@ -0,0 +1,22 @@
+export const Theme = {
+    light: '#F3f3f3',
+    dark: '#07070A',
+};
+
+export const FontStize = {
+    s: 13,
+    m: 17,
+    l: 20,
+    xl: 27,
+};
+
+export const ButtonColors = {
+    // Need to define
+};
+
+export const TextColor = {
+    d_white: '#FFFF',
+    d_blue : '#004BA8',
+    l_black: '#0700',
+    l_blue: '#084887'
+};
